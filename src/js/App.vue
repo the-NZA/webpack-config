b060ed7eb2c305236efa86f7463db2d0c0e2b08d
the_nza <the_nza@protonmail.com>
2020-10-22 12:42:57 +0300

    Some indent changes

--- src/js/App.vue
+++ src/js/App.vue
@@ -1,45 +1,44 @@
 <template>
-  <div id="app">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-    <p>some text</p>
-    <input v-model="test"
-    	type="text">
-	    <p>
-		{{ test }}
-	    </p>
-	    <button @click="testF">click me</button>
-  </div>
+	<div id="app">
+
+	<HelloWorld msg="Welcome to Your Vue.js App"/>
+	<p>some text</p>
+	<input v-model="test"
+		type="text">
+	<p> {{ test }} </p>
+	<button @click="testF">click me</button>
+
+	</div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  },
-  data(){
-	  return {
-		  test: ""
-	  }
-  },
-
-	  methods: {
-		  testF(){
-			  console.log("test method");
-		  }
-	  }
+	name: 'App',
+	components: {
+		HelloWorld
+	},
+	data(){
+		return {
+			test: ""
+		}
+	},
+	methods: {
+		testF(){
+			console.log("test method");
+		}
+	}
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+	font-family: Avenir, Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+	margin-top: 60px;
 }
 </style>
